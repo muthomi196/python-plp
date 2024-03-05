c72bc81cb1a9d2184d157407de408c54a01ed397
muthomi <mikebluez196@gmail.com>
2024-03-05 07:43:37 +0300

    loops = true

---
+++ loops.py
@@ -0,0 +1,16 @@
+#for loops
+names = ['Diana', 'maratha','joel','james']
+for name in names:
+    print(name)
+    print(len(name))
+
+#for loops using range
+Welcome='go home and stay there'
+for i in range(6):
+    print(Welcome[i])
+
+#while loops
+count=0
+while count<10:
+    print(count)
+    count+=1
